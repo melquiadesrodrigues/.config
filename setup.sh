db25743cfea32298bd8eb42f559183a05e2eebf5
Melquíades Rodrigues <melquiades.rodrigues@hotmail.com>
2025-06-20 13:57:11 +0100

    fix: setup sh syntax

--- setup.sh
+++ setup.sh
@@ -114,7 +114,7 @@ install_neovim() {
             curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
             sudo rm -rf /opt/nvim
             sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
-            echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> $HOME/.zshrc
+            echo "export PATH=\"\$PATH:/opt/nvim-linux-x86_64/bin\"" >> "$HOME/.zshrc"
         fi
     else
         echo "Neovim is already installed."
